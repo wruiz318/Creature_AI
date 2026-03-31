f328497876aefeae68921edb82538a85249a4b87
wruiz318 <wruiz318@student.fuhsd.org>
2026-03-31 15:28:09 -0700

    Partial completion of move to least moved tile

--- CreatureAITester/P7_Ruiz_William_Creature.java
+++ CreatureAITester/P7_Ruiz_William_Creature.java
@@ -1,6 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-import java.util.List;
-import java.util.TreeMap;
+import java.util.*;
 /**
  * Write a description of class PX_LastName_FirstName_Creature here.
  * 
@@ -18,6 +17,15 @@ public class P7_Ruiz_William_Creature extends Actor {
         }else{
             nodes.put(""+snapToGrid(getX())+"x"+snapToGrid(getY())+"y",nodes.get(""+snapToGrid(getX())+"x"+snapToGrid(getY())+"y") +1);
         }
+        if (!nodes.containsKey(snapToGrid(getX())+"x"+(snapToGrid(getY())-32)+"y")){
+            nodes.put(snapToGrid(getX())+"x"+(snapToGrid(getY())-32)+"y",0);
+       }if (!nodes.containsKey(snapToGrid(getX())+"x"+(snapToGrid(getY())+32)+"y")){
+           nodes.put(snapToGrid(getX())+"x"+(snapToGrid(getY())+32)+"y",0);
+       }if (!nodes.containsKey((snapToGrid(getX())-32)+"x"+(snapToGrid(getY()))+"y")){
+           nodes.put((snapToGrid(getX())-32)+"x"+(snapToGrid(getY()))+"y",0);
+       }if (!nodes.containsKey((snapToGrid(getX())+32)+"x"+(snapToGrid(getY()))+"y")){
+           nodes.put((snapToGrid(getX())+32)+"x"+(snapToGrid(getY()))+"y",0);
+       }
         List possibleTreats = this.getObjectsInRange(0,Treat.class);
         for (int i = 1;i<this.getWorld().getWidth();i++){
             possibleTreats = this.getObjectsInRange(i,Treat.class);
@@ -57,23 +65,60 @@ public class P7_Ruiz_William_Creature extends Actor {
            int downNodeValue = nodes.get(snapToGrid(getX())+"x"+(snapToGrid(getY())+32)+"y");
            int leftNodeValue = nodes.get((snapToGrid(getX())-32)+"x"+(snapToGrid(getY()))+"y");
            int rightNodeValue = nodes.get((snapToGrid(getX())+32)+"x"+(snapToGrid(getY()))+"y");
+           ArrayList<Integer> nodeValues = new ArrayList<Integer>();
+           nodeValues.add(upNodeValue);
+           nodeValues.add(downNodeValue);
+           nodeValues.add(leftNodeValue);
+           nodeValues.add(rightNodeValue);
+           ArrayList<String> nodeNames = new ArrayList<String>();
+           nodeNames.add(snapToGrid(getX())+"x"+(snapToGrid(getY())-32)+"y");
+           nodeNames.add(snapToGrid(getX())+"x"+(snapToGrid(getY())+32)+"y");
+           nodeNames.add((snapToGrid(getX())-32)+"x"+(snapToGrid(getY()))+"y");
+           nodeNames.add((snapToGrid(getX())+32)+"x"+(snapToGrid(getY()))+"y");
+           ArrayList<Integer> finalNodeValues = new ArrayList<Integer>();
+           for (String name : nodeNames){
+               if (getWorld().getObjectsAt(nodeNameToX(name) + 16,nodeNameToY(name) + 16,Wall.class).size() == 0){
+                   finalNodeValues.add(nodeValues.get(nodeNames.indexOf(name)));
+               }else{
+                   finalNodeValues.add(-1);
+               }
+           }
+           int lowest = finalNodeValues.get(0);
+           for (int i : finalNodeValues){
+               if (lowest == -1 || (lowest > i && i!=-1)){
+                   lowest = i;
+               }
+           }
+           String targetString;
+           targetString = nodeNames.get(finalNodeValues.indexOf(lowest));
+           targetX = nodeNameToX(targetString)+16;
+           targetY = nodeNameToY(targetString)+16;
+           /*
            while (true){
                int rand = Greenfoot.getRandomNumber(4) * 90;
                this.turn(rand);
                this.setLocation(getX()/32*32+16,getY()/32*32+16);
-               this.move(32);
+               for (int i= 0;i<16;i++){
+                   move(2);
+               }
                if (this.getOneIntersectingObject(Wall.class) == null){
                    targetY = this.getY()/32*32 + 16;
                    targetX = this.getX()/32*32 + 16;
-                   this.setLocation(beforeX,beforeY);
+                   this.turn(180);
+                   for (int i= 0;i<16;i++){
+                       move(2);
+                   }
                    this.setRotation(beforeRot);
                    break;
                }else{
-                   this.setLocation(beforeX,beforeY);
+                   this.turn(180);
+                   for (int i= 0;i<16;i++){
+                       move(2);
+                   }
                    this.setRotation(beforeRot);
                }
            }
-           
+           */
         }else if (targetY != -1 && targetX != -1){
             Point point = new Point();
             getWorld().addObject(point,targetX,targetY);
